7e99ec64101bdfb089deef014179bd566b4d9a10
nicolemaracaja <nicole.brito.maracaja@ccc.ufcg.edu.br>
2025-05-07 20:26:52 -0300

    aula3 springboot

---
+++ src/main/java/med/voll/api/medico/Especialidade.java
@@ -0,0 +1,10 @@
+package med.voll.api.medico;
+
+//Enum: Conjunto fixo de valores constantes
+public enum Especialidade {
+
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA;
+}
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,39 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id") //gera equals e hashcode em cima do id
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded //ficar numa classe separada, mas os campos fazem parte da mesma tabela
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}

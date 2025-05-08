6249d5a823401298e8599c6f84f9d5e76c601dd6
nicolemaracaja <nicole.brito.maracaja@ccc.ufcg.edu.br>
2025-05-08 10:09:56 -0300

    aula5Spring

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,23 +1,52 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
     @Autowired
-    private MedicoRepository repository;
+    private MedicoRepository repository; //herda de jpaRepository
 
     @PostMapping //enviar requisições
-    @Transactional
+    @Transactional //para salvar, atualizar ou excluir informações no bd
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping //leituras
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping //atualizações
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
+    //apaga de fato o registro do banco
+    /*@DeleteMapping("/{id}") //para excluir
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }*/
+
+    @DeleteMapping("/{id}") //para excluir
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
